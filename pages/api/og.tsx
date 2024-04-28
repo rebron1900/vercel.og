560676dab2252519af80d8572fc32049ef9356df
rebron1900 <me@1900.live>
2024-04-28 17:19:13 +0800

    Changes to be committed: 	modified:   pages/api/og.tsx

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -14,8 +14,11 @@ export default async function handler(request: NextRequest) {
         const { searchParams } = new URL(request.url);
 
         const originalTitle = searchParams.get('title');
+        const originalTags = searchParams.get('tags');
+        const originalDate = searchParams.get('date');
 
         let title = originalTitle;
+        let tags = originalTags;
 
         if (!title) {
             title = 'A Hugo blog about Charles Chin.';
@@ -23,92 +26,90 @@ export default async function handler(request: NextRequest) {
             title = title.slice(0, 100);
         }
 
+        if (!tags){
+            tags = '分享';
+        }else{
+            tags = tags.slice(0, 20);
+        }
+
         return new ImageResponse(
             (
-                <div
-                    style={{
-                        height: '100%',
-                        width: '100%',
-                        display: 'flex',
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '400px',
+                    border: 'solid 1px',
+                    justifyContent: 'flex-end',
+                    backgroundColor: 'white'
+                  }}>
+                    <div style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}>
+                      <span style={{
+                        backgroundColor: 'red',
+                        padding: '0px 15px',
+                        color: 'white',
+                        borderRadius: '5px',
+                        fontStyle: 'italic',
+                        letterSpacing: '3px',
+                      }}>{tags}</span>
+                    </div>
+                    <div style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      padding: '10px 0px 25px 0px',
+                      minHeight: '209px',
+                    }}>
+                      <p style={{
+                        fontSize: '41px',
+                        fontWeight: 'bolder',
+                        margin: '0',
+                        fontStyle: 'italic',
                         textAlign: 'center',
-                        alignItems: 'center',
-                        justifyContent: 'space-between',
-                        flexDirection: 'row',
-                        flexWrap: 'nowrap',
-                        backgroundColor: 'white',
-                        backgroundImage: 'radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
-                        backgroundSize: '100px 100px',
-                    }}
-                >
-                    <div
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <img
-                            alt="1900"
-                            height={256}
-                            src="https://cdn.1900.live/20190640/ico.png"
-                            style={{ margin: '0 60px 0 100px' }}
-                            width={256}
-                        />
+                        lineHeight: '1.2',
+                        padding: '0 35px',
+                      }}>{title}</p>
                     </div>
-                    <div
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            flexDirection: 'column',
-                            marginRight: '100px',
-                            alignItems: 'flex-start',
-                            width: '680px',
-                            minWidth: '680px'
-                        }}
-                    >
-                        <div
-                            style={{
-                                display: 'flex',
-                                fontSize: 48,
-                                fontStyle: 'normal',
-                                color: 'black',
-                                marginTop: 0,
-                                lineHeight: 1.8,
-                                whiteSpace: 'pre-wrap',
-                                marginRight: 'auto',
-                            }}
-                        >
-                            <b>只是玩玩&apos;Blog</b>
+                    <div style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      marginBottom: '16px',
+                    }}>
+                      <div style={{
+                        display: 'flex',
+                        marginLeft: '20px',
+                      }}>
+                        <img src="https://cdn.1900.live/20190640/ico.png" style={{
+                          width: '50px',
+                          height: '50px',
+                          borderRadius: '50%',
+                        }} />
+                        <div style={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          lineHeight: '1',
+                          justifyContent: 'center',
+                          fontSize: '14px',
+                          fontStyle: 'italic',
+                          fontWeight: '900',
+                          marginLeft: '13px',
+                        }}>
+                          <span style={{ fontSize: '20px' }}>@1900</span>
+                          <span>https://1900.live</span>
                         </div>
-                        <div
-                            style={{
-                                display: 'flex',
-                                fontSize: 40,
-                                fontStyle: 'normal',
-                                color: 'black',
-                                marginTop: 15,
-                                lineHeight: 1.8,
-                                whiteSpace: 'pre-wrap',
-                            }}
-                        >
-                            <b>《{title}》</b>
-                        </div>
-                        <div
-                            style={{
-                                display: 'flex',
-                                fontSize: 32,
-                                fontStyle: 'normal',
-                                color: 'dimgray',
-                                marginTop: 15,
-                                lineHeight: 1.8,
-                                whiteSpace: 'pre-wrap',
-                                marginLeft: 'auto',
-                            }}
-                        >
-                            <small>- 1900 (@1900)</small>
-                        </div>
+                      </div>
+                      <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <span style={{
+                          fontSize: '20px',
+                          marginRight: '20px',
+                          fontWeight: 'bold',
+                          fontStyle: 'italic',
+                        }}>{originalDate}</span>
+                      </div>
                     </div>
-                </div>
+                    <div style={{}} />
+                  </div>
             ),
             {
                 width: 1200,
@@ -117,7 +118,7 @@ export default async function handler(request: NextRequest) {
                     {
                         name: 'Maple UI',
                         data: fontData,
-                        style: 'normal',
+                        style: 'italic',
                     },
                 ],
             },
