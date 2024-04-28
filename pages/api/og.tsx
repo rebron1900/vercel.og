b50995c32c1a3bd9da1bd40625dda361a0ea47a7
rebron1900 <me@1900.live>
2024-04-28 18:00:49 +0800

    Changes to be committed: 	modified:   pages/api/og.tsx

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -15,15 +15,18 @@ export default async function handler(request: NextRequest) {
 
         const originalTitle = searchParams.get('title');
         const originalTags = searchParams.get('tags');
+        const originalDesc = searchParams.get('desc');
         const originalDate = searchParams.get('date');
 
         let title = originalTitle;
         let tags = originalTags;
+        let desc = originalDesc;
 
         if (!title) {
             title = 'A Hugo blog about Charles Chin.';
         } else {
-            title = title.slice(0, 20);
+            let dot = title.length >= 20 ? '...':'';
+            title = title.slice(0, 22) + dot;
         }
 
         if (!tags){
@@ -32,20 +35,24 @@ export default async function handler(request: NextRequest) {
             tags = tags.slice(0, 10);
         }
 
+        if(!desc){
+            desc = 'All work and no play makes Jack a dull boy'
+        }else{
+            desc = tags.slice(0, 100) + '...';
+        }
+
         return new ImageResponse(
             (
                 <div style={{
                     display: 'flex',
                     flexDirection: 'column',
                     height: '400px',
-                    width: '800px',
                     border: 'solid 1px',
-                    justifyContent: 'flex-end',
-                    backgroundColor: 'white'
+                    justifyContent: 'flex-end'
                   }}>
                     <div style={{
                       display: 'flex',
-                      justifyContent: 'center',
+                      justifyContent: 'center'
                     }}>
                       <span style={{
                         backgroundColor: 'red',
@@ -53,14 +60,16 @@ export default async function handler(request: NextRequest) {
                         color: 'white',
                         borderRadius: '5px',
                         fontStyle: 'italic',
-                        letterSpacing: '3px',
-                      }}>{tags}</span>
+                        letterSpacing: '3px'
+                      }}>
+                        {tags}
+                      </span>
                     </div>
                     <div style={{
                       display: 'flex',
-                      justifyContent: 'center',
-                      padding: '20px 0px 25px 0px',
+                      padding: '10px 0px 25px 0px',
                       minHeight: '209px',
+                      flexDirection: 'column'
                     }}>
                       <p style={{
                         fontSize: '41px',
@@ -69,23 +78,37 @@ export default async function handler(request: NextRequest) {
                         fontStyle: 'italic',
                         textAlign: 'center',
                         lineHeight: '1.2',
+                        padding: '0 35px'
+                      }}>
+                        {title}
+                      </p>
+                      <p style={{
+                        fontSize: '28px',
+                        fontWeight: 'bolder',
+                        marginTop: '10px',
+                        fontStyle: 'italic',
+                        textAlign: 'center',
+                        lineHeight: '1.2',
                         padding: '0 35px',
-                      }}>{title}</p>
+                        color: '#8b949e'
+                      }}>
+                        {desc}
+                      </p>
                     </div>
                     <div style={{
                       display: 'flex',
                       justifyContent: 'space-between',
-                      marginBottom: '16px',
+                      marginBottom: '16px'
                     }}>
                       <div style={{
                         display: 'flex',
-                        marginLeft: '20px',
+                        marginLeft: '20px'
                       }}>
                         <img src="https://cdn.1900.live/20190640/ico.png" style={{
                           width: '50px',
                           height: '50px',
-                          borderRadius: '50%',
-                        }} />
+                          borderRadius: '50%'
+                        }} alt="icon" />
                         <div style={{
                           display: 'flex',
                           flexDirection: 'column',
@@ -94,20 +117,30 @@ export default async function handler(request: NextRequest) {
                           fontSize: '14px',
                           fontStyle: 'italic',
                           fontWeight: '900',
-                          marginLeft: '13px',
+                          marginLeft: '13px'
                         }}>
-                          <span style={{ fontSize: '20px' }}>@1900 <br /></span>
+                          <span style={{
+                            fontSize: '20px'
+                          }}>
+                            @1900
+                          </span>
                         </div>
                       </div>
-                      <div style={{ display: 'flex', alignItems: 'center' }}>
+                      <div style={{
+                        display: 'flex',
+                        alignItems: 'center'
+                      }}>
                         <span style={{
                           fontSize: '20px',
                           marginRight: '20px',
                           fontWeight: 'bold',
-                          fontStyle: 'italic',
-                        }}>{originalDate}</span>
+                          fontStyle: 'italic'
+                        }}>
+                          2024/04/19
+                        </span>
                       </div>
                     </div>
+                    {/* The last div is empty and has no content or styles, so it's not included in the JSX */}
                   </div>
             ),
             {
